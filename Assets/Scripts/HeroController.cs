136f3d1079d02cb9784b0823068f17abec0e6e61
Joao Paiva <joaobapaiva@gmail.com>
2020-07-20 23:28:18 -0300

    Final Commit

---
+++ Assets/Scripts/HeroController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
+
+
+public class HeroController : MonoBehaviour
+{
+
+    public float speed = 5;
+
+    private Rigidbody2D body;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        body = GetComponent<Rigidbody2D>();
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 movimentVector = new Vector2(Input.GetAxisRaw("Horizontal"), 
+            Input.GetAxisRaw("Vertical"));
+
+        if(movimentVector != Vector2.zero)
+        {
+            var playerMovement = new Vector2(movimentVector.x, movimentVector.y) * speed;
+
+            body.velocity = playerMovement;
+        } 
+        else 
+        {
+            body.velocity = Vector2.zero;
+        }
+
+    }
+
+    void OnTriggerEnter2D(Collider2D other) {
+        if(other.name == "exit")
+        {
+            SceneManager.LoadScene("YouWin");
+        }
+    }
+}
---
+++ Assets/Scripts/YouWinController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class YouWinController : MonoBehaviour
+{
+    public AudioSource winnerSound;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        winnerSound = GetComponent<AudioSource>();
+    }
+     
+     
+    // called first
+    void OnEnable()
+    {
+        Debug.Log("OnEnable called");
+        SceneManager.sceneLoaded += OnSceneLoaded;
+    }
+
+    void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        winnerSound.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
